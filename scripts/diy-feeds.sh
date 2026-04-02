9cf93cf78359336ae4a88525b3bacacdbf9c22d0
xsdementevx <xsdementevx@gmail.com>
2026-04-03 02:32:55 +0700

    fix: add feed URL fallback and diagnostics for passwall2

--- scripts/diy-feeds.sh
+++ scripts/diy-feeds.sh
@@ -7,5 +7,16 @@ echo ">> Adding passwall feeds..."
 echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >> feeds.conf.default
 echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main' >> feeds.conf.default
 
+# Verify feeds can be cloned (fail early with useful error)
+echo ">> Testing passwall2 repo accessibility..."
+git ls-remote --exit-code https://github.com/xiaorouji/openwrt-passwall2.git main || {
+  echo ">> xiaorouji/openwrt-passwall2 not accessible, trying Openwrt-Passwall org..."
+  sed -i 's|xiaorouji/openwrt-passwall2|Openwrt-Passwall/openwrt-passwall2|' feeds.conf.default
+}
+git ls-remote --exit-code https://github.com/xiaorouji/openwrt-passwall-packages.git main || {
+  echo ">> xiaorouji/openwrt-passwall-packages not accessible, trying Openwrt-Passwall org..."
+  sed -i 's|xiaorouji/openwrt-passwall-packages|Openwrt-Passwall/openwrt-passwall-packages|' feeds.conf.default
+}
+
 echo ">> feeds.conf.default:"
 cat feeds.conf.default
